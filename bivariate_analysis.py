28f925df624bc6ffa343899da5df6bc369e74966
Altaf0786 <altafshekh27932@gmail.com>
2024-11-08 22:09:39 +0530

    updated code

--- bivariate_analysis.py
+++ bivariate_analysis.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import seaborn as sns
 from scipy.stats import chi2_contingency, ttest_ind, f_oneway, spearmanr, kendalltau
 import statsmodels.api as sm
